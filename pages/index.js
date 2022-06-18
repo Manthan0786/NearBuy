4ea69384e350082c7f4750c5a138baa2154a3bd5
Manthan Bhardwaj <95605134+Manthan0786@users.noreply.github.com>
2022-06-17 20:14:16 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Header from "../src/components/Header/header"
+
+export default function App() {
+  return (
+
+    <div>
+      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Playfair+Display" />
+      <Header />
+    </div>
+  )
+}
