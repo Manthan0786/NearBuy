167dfc04bb28fec310e4c52bf8f584164191a422
Manthan Bhardwaj <95605134+Manthan0786@users.noreply.github.com>
2023-05-03 23:38:33 -0600

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,18 @@
 import '../public/css/styles.css'
 import Header from '../src/components/BuyerHeader/header'
-//import { Provider } from "react-redux";
-import { store, persistor } from '../src/components/store';
+import { store, Provider } from "react-redux";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  persistor.persist()
-  if (Component.name === "Login") {
-    return (
-      <><Component {...pageProps} /></>
-    )
-  } else if (Component.name === "Signup") {
-    return (
-      <><Component {...pageProps} /></>
-    )
-  }
+  // if (Component.name === "Login") {
+  //   return (
+  //     <><Component {...pageProps} /></>
+  //   )
+  // } else if (Component.name === "Signup") {
+  //   return (
+  //     <><Component {...pageProps} /></>
+  //   )
+  // }
   return (
     <>
       <Provider store={store}>
